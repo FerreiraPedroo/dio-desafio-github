84f17c324c25e2547c5c188b9fa1951690f7d835
Pedro Henrique de Assis Ferreira <73146680+FerreiraPedroo@users.noreply.github.com>
2022-04-13 21:58:53 -0300

    Create script.js

---
+++ Desafio/script.js
@@ -0,0 +1,7 @@
+let N = 13;
+for (let i = 2; i <= 10000; i++) {
+	if (N > 0 && i % N === 2) console.log(i);
+}
+// for (let i = 2; i < 10000; i++) {
+//   if (i % N === 2) console.log("I:",i);
+//  }
